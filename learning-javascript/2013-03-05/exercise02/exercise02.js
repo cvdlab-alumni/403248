b8829ffea148940b0778c01eee5066acb7d2bbac
miccia4 <mcannaviccio@gmail.com>
2013-03-06 12:27:59 +0100

    esercizi aggiunti

---
+++ learning-javascript/2013-03-05/exercise02/exercise02.js
@@ -0,0 +1,13 @@
+var riga;
+riga='';
+
+for(var i=1; i<=10; i++){
+	for(var j=1; j<=10; j++){
+		if (j===1) {
+			riga=i*j;
+		} else{
+		riga=riga + ',\t' + i*j;
+		};
+	};
+	console.log(riga);
+};
---
+++ learning-javascript/2013-03-05/exercise03/exercise03.js
@@ -0,0 +1,18 @@
+var riga;
+var n;
+
+for(var i=1; i<=10; i++){
+	for(var j=1; j<=10; j++){
+		if (i===j) {
+			n=1;
+		}else{
+			n=0;
+		};
+		if (j===1){
+			riga=n;
+		}else{
+		riga=riga + '\t' + n;
+		};
+	}
+	console.log(riga);
+}
